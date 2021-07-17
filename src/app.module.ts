59c68c02b344527758fc728e31de35258232b993
marcelobiao <biao.marcelo@gmail.com>
2021-07-17 14:40:29 -0300

    Ajustando app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
+import { Livro } from './livro.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
